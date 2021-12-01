1f12dc7430006da242cabbe16d15d66e50a6b54f
Hugo Guittenit <guitteni1u@etu.univ-lorraine.fr>
2021-12-02 00:20:27 +0100

    modif main

--- src/TestVotingProject.java
+++ src/TestVotingProject.java
@@ -2,29 +2,15 @@ import java.util.ArrayList;
 
 import modele.Item;
 import modele.Tirage;
+import vue.VotingProjectWindow;
 
 public class TestVotingProject {
 
 	public static void main(String[] args) {
-		ArrayList<Item> listeItemsDeVote= new ArrayList<>();
-
-		listeItemsDeVote.add(new Item("item1",16, 10));
-		listeItemsDeVote.add(new Item("item2",1, 20));
-		listeItemsDeVote.add(new Item("item3",1, 15));
-		listeItemsDeVote.add(new Item("item4",8, 30));
-		listeItemsDeVote.add(new Item("item5",32, 50));
-		listeItemsDeVote.add(new Item("item6",1, 35));
-		listeItemsDeVote.add(new Item("item7",4, 40));
-		listeItemsDeVote.add(new Item("item8",64, 5));
-		listeItemsDeVote.add(new Item("item9",16, 25));
-		
 		Tirage tirage = Tirage.getInstance();
-		tirage.setItems(listeItemsDeVote);
+		VotingProjectWindow.getInstance().show();
+
 		//System.out.println(tirage);
-		
-		System.out.println(tirage.getResultatDernierTirage());
-		Tirage.getInstance().effectuerTirages(1500);
-		System.out.println(tirage.getResultatDernierTirage());
-		
+
 	}
 }
