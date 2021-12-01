1c3d8fb71363c35a53ef6ca2faf7349f095913de
Hugo Guittenit <guitteni1u@etu.univ-lorraine.fr>
2021-12-01 17:36:51 +0100

    séparation de chargerTirage de ExpertChargementCSV

--- src/modele/gestionFichiers/experts/chargement/ExpertChargementCSV.java
+++ src/modele/gestionFichiers/experts/chargement/ExpertChargementCSV.java
@@ -2,6 +2,7 @@ package modele.gestionFichiers.experts.chargement;
 
 import modele.Item;
 import modele.gestionFichiers.experts.ExpertCOR;
+import modele.gestionFichiers.metier.gestionFichierCSV;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -17,9 +18,9 @@ public class ExpertChargementCSV extends ExpertCOR {
     protected ArrayList<Item> resoudre1(Object o) {
         try {
             if (o instanceof String) {
-                String v = (String) o;
-                if (v.endsWith(".csv"))
-                    return chargerFichier(v);
+                String path = (String) o;
+                if (path.endsWith(".csv"))
+                    return new gestionFichierCSV().charger(path).getItems();
                 return null;
             }
             else
@@ -27,35 +28,5 @@ public class ExpertChargementCSV extends ExpertCOR {
         }catch (IllegalArgumentException e) {
             return null;
         }
-    }
-
-    private ArrayList<Item> chargerFichier(String path){
-        try{
-            Scanner reader = new Scanner(new File(path));
-            ArrayList<Item> items = new ArrayList<>();
-            if(reader.hasNextLine()){
-                reader.nextLine();
-                ArrayList<String> res = new ArrayList<>();
-                String line;
-                String[] resList;
-                while(reader.hasNextLine()){
-                    line = reader.nextLine();
-                    resList = line.contains(";") ? line.split(";") : line.split(",");
-                    res = new ArrayList<>(Arrays.asList(resList));
-                    items.add(
-                            new Item(
-                                    res.get(0),
-                                    Integer.parseInt(res.get(1)),
-                                    Double.parseDouble(res.get(2).replaceFirst(",","."))
-                            )
-                    );
-                }
-                reader.close();
-                return items;
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-        return null;
     }
 }
---
+++ src/modele/gestionFichiers/metier/gestionFichierCSV.java
@@ -0,0 +1,84 @@
+package modele.gestionFichiers.metier;
+
+import modele.Item;
+import modele.Tirage;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class gestionFichierCSV extends gestionFichier{
+
+    @Override
+    public Tirage charger(String path){
+        try{
+            Scanner reader = new Scanner(new File(path));
+            Tirage t = new Tirage(new ArrayList<>());
+            if(reader.hasNextLine()){
+                String line = reader.nextLine();
+                if(line.matches(".*[0-9]+.*")) stringIntoTirage(line, t);
+                while(reader.hasNextLine()){
+                    line = reader.nextLine();
+                    stringIntoTirage(line, t);
+                }
+                reader.close();
+                return t;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void stringIntoTirage(String line, Tirage t){
+        ArrayList<String> res = new ArrayList<>();
+        String[] resList;
+        resList = line.contains(";") ? line.split(";") : line.split(",");
+        res = new ArrayList<>(Arrays.asList(resList));
+        t.AjouterItem(
+                new Item(
+                        res.get(0),
+                        Integer.parseInt(res.get(1)),
+                        Double.parseDouble(res.get(2).replaceFirst(",",".").replaceAll("\"",""))
+                )
+        );
+    }
+
+    @Override
+    public boolean sauvegarder(Tirage t, String path) {
+        if(path.isBlank())
+        try{
+            FileWriter writer = new FileWriter(path);
+            boolean hasDernierTirage = t.getResultatDernierTirage() != "Aucun tirage effectué dernièrement";
+
+            ArrayList<Item> tItems = t.getItems();
+            ArrayList<Integer> tDernierTirage = t.getNbItems();
+            double probaGlobale = t.getProbaGlobale();
+            Item item;
+
+            writer.write("Nom,Quantité,Probabilité");
+            if(hasDernierTirage)
+                writer.write(",Résultats dernier tirage");
+            writer.write("\n");
+            for(int i = 0; i < t.getItems().size(); i++){
+                item = tItems.get(i);
+                writer.write(
+                        item.getNom()+","+
+                                item.getQuantite()+",\""+
+                                new BigDecimal(item.getProbabilite()/probaGlobale).toPlainString().replaceFirst("\\.", ",") +"\""
+                );
+                if(hasDernierTirage)
+                    writer.write(","+tDernierTirage.get(i));
+                writer.write("\n");
+            }
+            writer.close();
+            return true;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
