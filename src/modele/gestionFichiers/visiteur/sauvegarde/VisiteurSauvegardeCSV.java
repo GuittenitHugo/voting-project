501b25b777ed9ba6bb879ebf3046e4705ebcb14b
Hugo Guittenit <guitteni1u@etu.univ-lorraine.fr>
2021-12-01 17:37:26 +0100

    ajout Sauvegarde format csv google sheets

---
+++ src/modele/gestionFichiers/visiteur/sauvegarde/VisiteurSauvegardeCSV.java
@@ -0,0 +1,17 @@
+package modele.gestionFichiers.visiteur.sauvegarde;
+
+import modele.Tirage;
+import modele.gestionFichiers.metier.gestionFichierCSV;
+import modele.gestionFichiers.visiteur.Visiteur;
+
+public class VisiteurSauvegardeCSV extends Visiteur {
+
+    String path;
+
+    public VisiteurSauvegardeCSV(String path){
+        super();
+        this.path = path;
+    }
+    @Override
+    public void visite(Tirage t) { new gestionFichierCSV().sauvegarder(t, path); };
+}
