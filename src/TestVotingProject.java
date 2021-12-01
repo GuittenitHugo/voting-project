50bb7050274b8f6dd88c73208a74d1d3ec7fb10b
Hugo Guittenit <guitteni1u@etu.univ-lorraine.fr>
2021-12-01 17:37:42 +0100

    modif main pour tests

--- src/TestVotingProject.java
+++ src/TestVotingProject.java
@@ -5,6 +5,7 @@ import modele.Item;
 import modele.Tirage;
 import modele.gestionFichiers.experts.Expert;
 import modele.gestionFichiers.experts.chargement.ExpertChargementCSV;
+import modele.gestionFichiers.visiteur.sauvegarde.VisiteurSauvegardeCSV;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -17,17 +18,24 @@ public class TestVotingProject {
 
 		Expert expertChargement = new ExpertChargementCSV(null);
 
-		FileDialog fDialog = new FileDialog((Frame)null, "Choisir un fichier à ouvrir");
+		FileDialog fDialog = new FileDialog((Frame)null, "");
+		fDialog.setTitle("Choisir un fichier à charger");
 		fDialog.setMode(FileDialog.LOAD);
 		fDialog.setVisible(true);
 
 		tirage.setItems((ArrayList<Item>) expertChargement.resoudre(fDialog.getDirectory()+fDialog.getFile()));
 
 		System.out.println(tirage.getResultatDernierTirage());
+
 		tirage.effectuerTirages(1500);
 		System.out.println(tirage.getResultatDernierTirage());
+		System.out.println(tirage);
 
-		fDialog.setVisible(false);
+		fDialog.setTitle("Choisir ou sauvegarder le fichier");
+		fDialog.setMode(FileDialog.SAVE);
+		fDialog.setVisible(true);
+
+		tirage.accepteSauvegarde(new VisiteurSauvegardeCSV(fDialog.getDirectory()+fDialog.getFile()));
 
 		System.exit(0);
 	}
