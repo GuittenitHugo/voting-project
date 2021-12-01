b326a5688a4587ee97270cca467f151fd651a750
Hugo Guittenit <guitteni1u@etu.univ-lorraine.fr>
2021-12-02 00:18:43 +0100

    creation listener

--- src/vue/actionListeners/sauverBtnListener.java
+++ src/vue/actionListeners/sauverBtnListener.java
@@ -4,6 +4,7 @@ import modele.Item;
 import modele.Tirage;
 import modele.gestionFichiers.experts.Expert;
 import modele.gestionFichiers.experts.chargement.ExpertChargementCSV;
+import modele.gestionFichiers.visiteur.sauvegarde.VisiteurSauvegardeCSV;
 import vue.VotingProjectWindow;
 
 import java.awt.*;
@@ -11,27 +12,23 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class chargerBtnListener implements ActionListener {
+public class sauverBtnListener implements ActionListener {
 
-    private Expert expertChargement;
-
-    public chargerBtnListener() {
-        expertChargement = new ExpertChargementCSV(null);
+    public sauverBtnListener() {
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Tirage t = Tirage.getInstance();
         VotingProjectWindow window = VotingProjectWindow.getInstance();
         ArrayList<String> list = new ArrayList<>();
-        expertChargement = new ExpertChargementCSV(null);
+        Expert expertChargement = new ExpertChargementCSV(null);
 
         FileDialog fDialog = new FileDialog(window, "");
-        fDialog.setTitle("Ouvrir");
-        fDialog.setMode(FileDialog.LOAD);
+        fDialog.setTitle("Sauvegarder");
+        fDialog.setMode(FileDialog.SAVE);
         fDialog.setVisible(true);
 
-        t.setItems((ArrayList<Item>) expertChargement.resoudre(fDialog.getDirectory()+fDialog.getFile()));
-        window.updateList(window.convertTirageToArray());
+        Tirage.getInstance().accepteSauvegarde(new VisiteurSauvegardeCSV(fDialog.getDirectory()+fDialog.getFile()));
+
     }
 }
